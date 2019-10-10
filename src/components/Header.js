5362ddde21744e8e1f8fd8ad462b96f9a6ba0391
yulin ye <ericye1001@gmail.com>
2019-10-10 19:16:23 +1000

    Add components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <img src="icons/logo.png"/>
+            </header>
+        );
+    }
+}
+
+export default Header
